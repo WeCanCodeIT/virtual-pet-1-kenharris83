a8aad701bda4e0fae851cb2f7211e04b188a5dfc
kenharris83 <kenn.harris83@gmail.com>
2023-12-11 21:17:27 -0500

    Created and save virtual pet homework.

--- src/VirtualPet.java
+++ src/VirtualPet.java
@@ -1,3 +1,78 @@
 public class VirtualPet{
+private String name;
+private int energy;
+private int hunger;
+private int thirst;
 
+
+public VirtualPet(String name, int energy, int hunger, int thirst) {
+    this.name = name;
+    this.energy = energy;
+    this.hunger = hunger;
+    this.thirst = thirst;
+}
+
+public String getName() {
+    return name;
+}
+
+public void setName(String name) {
+    this.name = name;
+}
+
+public int getEnergy() {
+    return energy;
+}
+
+public void setEnergy(int energy) {
+    this.energy = energy;
+}
+
+public int getHunger() {
+    return hunger;
+}
+
+public void setHunger(int hunger) {
+    this.hunger = hunger;
+}
+
+public int getThirst() {
+    return thirst;
+}
+
+public void setThirst(int thirst) {
+    this.thirst = thirst;
+}
+
+public void tick(){
+    this.hunger -= 1;
+    this.thirst -= 1;
+    this.energy -= 1;
+    }
+
+public void petStats(){
+    System.out.println("Name: " +this.name);
+    System.out.println("Hunger level: " +this.hunger);
+    System.out.println("Thirst level: " +this.thirst);
+    System.out.println("Energy level: " +this.energy);
+    
+}
+
+public void feedPet(){
+    this.hunger +=5;
+    this.energy +=2;
+    this.thirst -=2;
+}
+
+public void waterPet(){
+    this.hunger -=2;
+    this.energy -=1;
+    this.thirst +=6;
+}
+
+public void playWithPet(){
+    this.hunger -=3;
+    this.energy -=4;
+    this.thirst +=3;
+}
 }
--- src/VirtualPetApp.java
+++ src/VirtualPetApp.java
@@ -1,5 +1,58 @@
+import java.util.Scanner;
+
 public class VirtualPetApp {
     public static void main(String[] args) {
+        int select;
+
+        Scanner userInput = new Scanner(System.in);
+        System.out.println( "Have you decided on a name for your pet?");
+        String name = userInput.nextLine();
         
+        VirtualPet pet = new VirtualPet(name, 5, 2, 3);
+
+        System.out.println("Since you are now a proud owner of " +name+ ", there are a few things you must know. The pet will need food, water, exercise and lots of love. If you neglect your pet they will surely die and we do not want that.");
+        System.out.println("Be sure to check on your pet often.");
+
+        do{
+            System.out.println("How will you be taking care of your pet today?");
+            System.out.println("1. Feed "+ name +" some food." );
+            System.out.println("2. Give "+ name +" some water." );
+            System.out.println("3. Play games with "+ name );
+            System.out.println("4. Exit" );
+
+            select = Integer.parseInt(userInput.nextLine());
+
+            switch (select) {
+                case 1:
+                    pet.feedPet();
+                    System.out.println(pet.getName()+ " has been fed");
+                    System.out.println(pet.getName()+ " has new stats ");
+                    System.out.println(pet.getName()+ "energy is: " +pet.getEnergy());
+                    System.out.println(pet.getName()+ "hunger is: " +pet.getHunger());
+                    System.out.println(pet.getName()+ "energy is: " +pet.getEnergy());
+                    break;
+                case 2:
+                    pet.waterPet();
+                    System.out.println("You gave water to " +name);
+                    System.out.println(pet.getName()+ " has new stats ");
+                    System.out.println(pet.getName()+ "energy is: " +pet.getEnergy());
+                    System.out.println(pet.getName()+ "hunger is: " +pet.getHunger());
+                    System.out.println(pet.getName()+ "energy is: " +pet.getEnergy());
+                case 3:
+                    pet.playWithPet();
+                    System.out.println("You played with " +name);
+                    System.out.println(pet.getName()+ " has new stats ");
+                    System.out.println(pet.getName()+ "energy is: " +pet.getEnergy());
+                    System.out.println(pet.getName()+ "hunger is: " +pet.getHunger());
+                    System.out.println(pet.getName()+ "energy is: " +pet.getEnergy());
+                default:
+                    break;
+            }
+
+            pet.tick();
+        }while(select != 4);
+
+
     }
+    
 }
